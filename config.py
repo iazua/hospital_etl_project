fa92f04ba2977a24b08e54cfdead62f92ed47259
Ignacio Andres Azua Zuniga <79027661+iazua@users.noreply.github.com>
2026-02-28 15:13:03 -0300

    Update config.py

--- config.py
+++ config.py
@@ -30,6 +30,7 @@ REPORTS_DIR     = os.path.join(BASE_DIR, "data", "reports")
 # CORREO (se usará en Fase 6)
 # ---------------------------------------------
 EMAIL_SENDER        = "ignacio.azua95@gmail.com"
-EMAIL_PASSWORD      = "kraj xqso qogu qqqj"       # Contraseña de aplicación Gmail
+EMAIL_PASSWORD      = ""       
 EMAIL_RECIPIENTS    = ["ignacio.azua@outlook.com"]
+
 EMAIL_SUBJECT       = "Reporte Diario - Estadísticas Hospital Metropolitano"
